4a91c3732bc0abe51e8b6c63bb5752b1acb43378
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-05-25 23:23:05 +0530

    nevermind: Add nevermind plugin
    Closes https://github.com/coala/corobo/issues/4

---
+++ plugins/nevermind.py
@@ -0,0 +1,13 @@
+import re
+
+from errbot import BotPlugin, re_botcmd
+
+
+class Nevermind(BotPlugin):
+    """
+    Doesn't mind
+    """
+
+    @re_botcmd(pattern=r"(nm)|(nevermind)", flags=re.IGNORECASE)
+    def nevermind(self, message, match):
+        return "I'm sorry :("
---
+++ tests/nevermind_test.py
@@ -0,0 +1,9 @@
+pytest_plugins = ['errbot.backends.test']
+
+extra_plugin_dir = ['plugins']
+
+def test_nevermind(testbot):
+    testbot.assertCommand("!nevermind", "I'm sorry :(")
+    testbot.assertCommand("!nm", "I'm sorry :(")
+    testbot.assertCommand("!nEverMINd", "I'm sorry :(")
+    testbot.assertCommand("!nM", "I'm sorry :(")
