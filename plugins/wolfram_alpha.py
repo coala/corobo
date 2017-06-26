7c3ac8028dc918bad59825591c514025ee84237c
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-06-26 14:57:23 +0530

    wolfram_alpha: Check if the pods exist
    There are questions/strings that doesn't yield any pods at all, which raise
    KeyError.
    
    Fixes https://github.com/coala/corobo/issues/161

--- plugins/wolfram_alpha.py
+++ plugins/wolfram_alpha.py
@@ -19,10 +19,13 @@ class WolframAlpha(BotPlugin):
         """
         Query the Computational Knowledge Engine.
         """
-        res = self.client.query(arg)
         ans = ''
-        for pod in res.pods:
-            if pod.title in ['Result', 'Results']:
-                for sub in pod.subpods:
-                    ans += sub.plaintext
+        res = self.client.query(arg)
+        try:
+            for pod in res.pods:
+                if pod.title in ['Result', 'Results']:
+                    for sub in pod.subpods:
+                        ans += sub.plaintext
+        except AttributeError:
+            self.log.info('KeyError triggered on retrieving pods.')
         return ans if ans else 'Dunno :('
--- tests/wolfram_alpha_test.py
+++ tests/wolfram_alpha_test.py
@@ -1,12 +1,27 @@
-from plugins.wolfram_alpha import WolframAlpha
+import unittest
+import logging
 
 import vcr
+from errbot.backends.test import TestBot
 
-pytest_plugins = ['errbot.backends.test']
-extra_plugin_dir = 'plugins'
+from plugins.wolfram_alpha import WolframAlpha
 
-my_vcr = vcr.VCR(match_on=['method', 'scheme', 'host', 'port', 'path'])
+my_vcr = vcr.VCR(match_on=['method', 'scheme', 'host', 'port', 'path'],
+                 filter_query_parameters=['appid'])
 
-@my_vcr.use_cassette('tests/cassettes/wa.yaml')
-def test_wa(testbot):
-    testbot.assertCommand("!wa 2^6", "64")
+
+class WolframAlphaTest(unittest.TestCase):
+
+    def setUp(self):
+        self.testbot = TestBot(extra_plugin_dir='plugins',
+                               loglevel=logging.ERROR)
+        self.testbot.start()
+
+    @my_vcr.use_cassette('tests/cassettes/wa.yaml')
+    def test_wa(self):
+        self.testbot.assertCommand("!wa 2^6", "64")
+        with self.assertLogs() as cm:
+            self.testbot.assertCommand('!wa this is a sentence',
+                                       'Dunno')
+        self.assertIn('INFO:errbot.plugins.wolfram alpha:KeyError triggered on '
+                      'retrieving pods.', cm.output)
