6f41a7b2d2256bfea1dfad1b7e8b008bd1ac983c
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-06-27 07:40:51 +0700

    Add deployment script
    This script is in charge of doing the actual deployment and it's
    executed from the server.
    
    Related to https://github.com/coala/corobo/issues/156

---
+++ .ci/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Continuous deployment to solar.coala.io
+# This script is meant to run on solar.coala.io
+
+# Execute this script in a CI environment with
+# the required SSH added to the agent, with
+# this command:
+# $ ssh solar.coala.io "bash -s" < /path/to/script
+
+WORK_DIR=/opt/solar/corobo
+
+fail() {
+    echo "FAIL: $*"
+    exit 1
+}
+
+cd $WORK_DIR || \
+    fail "It looks like $WORK_DIR doesn't exist"
+
+docker-compose pull || \
+    fail "An error has occured during updating the image(s)"
+
+docker-compose up --force-recreate -d || \
+    fail "An error has occured during deploying the container(s)"
+
+sleep 5s
+
+docker-compose ps
