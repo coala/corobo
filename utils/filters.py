7dcc1e07a4b4f795b2d8de9cea324b7de860d76f
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-07-07 02:10:09 +0530

    filters: Filter echo cmd from coala and bears
    Closes https://github.com/coala/corobo/issues/195
    Closes https://github.com/coala/corobo/issues/194

---
+++ utils/filters.py
@@ -0,0 +1,23 @@
+import re
+
+from errbot import BotPlugin, cmdfilter
+
+
+class Filters(BotPlugin):
+    """
+    Filter and manipulate incoming commands
+    """
+
+    @cmdfilter
+    def filters(self, msg, cmd, args, dry_run):
+        # Blacklisting commands from specified rooms
+        CMDS_TO_BLACKLIST = {
+            'coala/coala': ['echo'],
+            'coala/coala-bears': ['echo'],
+            'coala/corobo': ['echo']
+        }
+
+        for room, commands in CMDS_TO_BLACKLIST.items():
+            if cmd in commands and msg.frm.room.uri == room:
+                return None, None, None
+        return msg, cmd, args
