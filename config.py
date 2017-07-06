e942acae5fb2f985fde3c939e884b0bba3929fd5
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-07-06 20:13:44 +0530

    config: Make bot admins, rooms & backend cfgable

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 COBOT_ROOT = os.environ.get('COBOT_ROOT', os.getcwd())
 
-BACKEND = 'Gitter'
+BACKEND = os.environ.get('BACKEND', 'Gitter')
 
 BOT_EXTRA_BACKEND_DIR = os.path.join(COBOT_ROOT, 'err-backend-gitter')
 
@@ -17,7 +17,7 @@ BOT_LOG_LEVEL = logging.DEBUG
 
 BOT_PREFIX = os.environ.get('COBOT_PREFIX', 'corobo ')
 
-BOT_ADMINS = ('meetmangukiya', )
+BOT_ADMINS = os.environ.get(tuple('BOT_ADMINS'.split()), ('meetmangukiya', ))
 
 BOT_IDENTITY = {
     'token': os.environ['COBOT_TOKEN']
@@ -25,19 +25,20 @@ BOT_IDENTITY = {
 
 DIVERT_TO_PRIVATE = ('help', )
 
-CHATROOM_PRESENCE = (
-    'coala/coala',
-    'coala/coala/offtopic',
-    'coala/cobot-test',
-    'coala/corobo',
-    'coala/devops',
-    'coala/community',
-    'coala/coala/gsoc',
-    'coala/coala/maintainers',
-    'coala/coala-bears',
-    'coala/bearship',
-    'coala/gci',
-    'coala/cobot'
+CHATROOM_PRESENCE = os.environ.get(
+    tuple(os.environ.get('ROOMS').split()),
+    ('coala/coala',
+     'coala/coala/offtopic',
+     'coala/cobot-test',
+     'coala/corobo',
+     'coala/devops',
+     'coala/community',
+     'coala/coala/gsoc',
+     'coala/coala/maintainers',
+     'coala/coala-bears',
+     'coala/bearship',
+     'coala/gci',
+     'coala/cobot')
 )
 
 AUTOINSTALL_DEPS = True
