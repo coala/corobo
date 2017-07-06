f0c6725242c3e409294a7da18c387eb13716f64f
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-07-06 23:39:17 +0530

    constants: Make org names configurable

--- plugins/constants.py
+++ plugins/constants.py
@@ -1,5 +1,7 @@
+import os
+
 API_DOCS = 'http://api.coala.io/en/latest'
 USER_DOCS = 'http://docs.coala.io/en/latest'
 
-GH_ORG_NAME = 'coala'
-GL_ORG_NAME = 'coala'
+GH_ORG_NAME = os.environ.get('GH_ORG_NAME', 'coala')
+GL_ORG_NAME = os.environ.get('GL_ORG_NAME', 'coala')
