312626b2093a6d29929991284ba3edf0e5c277b2
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-08-30 01:31:54 +0530

    semaphore.answers: Check only for non-master
    - `pull_and_build` function is created.
    - Check is only performed for non-master branches, the image should
      always be built for master builds.
    
    Fixes https://github.com/coala/corobo/issues/281

--- .ci/semaphore.answers.sh
+++ .ci/semaphore.answers.sh
@@ -1,6 +1,16 @@
 pip install gitpython
-.ci/check_docker.py
-if [[ $? == 1 ]]
-then docker pull meetmangukiya/corobo-answers:latest && \
-     docker build -t meetmangukiya/corobo-answers answers/
+
+pull_and_build() {
+    docker pull meetmangukiya/corobo-answers:latest
+    docker build -t meetmangukiya/corobo-answers answers/
+}
+
+if [[ $BRANCH_NAME != "master" ]]
+then
+    .ci/check_docker.py
+    if [[ $? == 1 ]]
+    then pull_and_build
+    fi
+else
+    pull_and_build
 fi
