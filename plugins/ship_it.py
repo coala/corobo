6b6a02f5288aa3947d8bf3abfa009fd493f99eeb
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-02 22:19:29 +0530

    ship_it: Remove 404'ing images

--- plugins/ship_it.py
+++ plugins/ship_it.py
@@ -15,10 +15,7 @@ class Ship_it(BotPlugin):
         'http://d2f8dzk2mhcqts.cloudfront.net/0772_PEW_Roundup/09_Squirrel.jpg',
         'http://www.cybersalt.org/images/funnypictures/s/supersquirrel.jpg',
         'http://www.zmescience.com/wp-content/uploads/2010/09/squirrel.jpg',
-        'https://dl.dropboxusercontent.com/u/602885/github/sniper-squirrel.jpg',
         'http://1.bp.blogspot.com/_v0neUj-VDa4/TFBEbqFQcII/AAAAAAAAFBU/E8kPNmF1h1E/s640/squirrelbacca-thumb.jpg',
-        'https://dl.dropboxusercontent.com/u/602885/github/soldier-squirrel.jpg',
-        'https://dl.dropboxusercontent.com/u/602885/github/squirrelmobster.jpeg',
     ]
     # stop ignoring LineLengthBear PyCodeStyleBear
 
