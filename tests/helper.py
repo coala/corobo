a312a1fb4756e58f51cb6db5182fb0ee39f3c299
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-10-29 00:08:07 +0530

    helper: Add extra configuration option to helper

--- tests/helper.py
+++ tests/helper.py
@@ -1,7 +1,8 @@
 from errbot.backends.test import TestBot
 
-def plugin_testbot(klass, loglevel):
-    testbot = TestBot(loglevel=loglevel)
+def plugin_testbot(klass, loglevel, config=None):
+    config = config if config else dict()
+    testbot = TestBot(loglevel=loglevel, extra_config=config)
     testbot.start()
     plug = testbot.bot.plugin_manager.instanciateElement(klass)
     return plug, testbot
