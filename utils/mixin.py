e2c46d78cc4efe2d8778b0580b37ac95299c4ee1
Nitanshu <nitanshu.vzard@gmail.com>
2018-07-21 05:34:16 +0530

    configuration: Implement DefaultConfigMixin
    It allows configuration of plugins from config.py
    so that admins won't have to configure the plugin
    everytime the bot is deployed.
    
    Closes https://github.com/coala/corobo/issues/586

---
+++ utils/mixin.py
@@ -0,0 +1,38 @@
+from itertools import chain
+
+class DefaultConfigMixin():
+
+    @property
+    def _default_config(self):
+        if (hasattr(self.bot_config, 'DEFAULT_CONFIG') and
+                self.name in self.bot_config.DEFAULT_CONFIG):
+            return self.bot_config.DEFAULT_CONFIG[self.name]
+
+    def __init__(self, bot, name=None):
+        super().__init__(bot, name=name)
+        default_config = self._default_config
+        if default_config and not hasattr(self, 'config'):
+            self.configure(default_config)
+
+    def get_configuration_template(self):
+        default_config = self._default_config
+        if default_config:
+            return default_config
+        elif self.CONFIG_TEMPLATE:
+            return self.CONFIG_TEMPLATE
+
+    def configure(self, configuration):
+        default_config = self._default_config
+        if configuration and default_config:
+            config = dict(chain(
+                default_config.items(),
+                configuration.items()))
+        elif configuration:
+            config = dict(chain(self.CONFIG_TEMPLATE.items(),
+                          configuration.items()))
+        elif default_config:
+            config = default_config
+        else:
+            config = self.CONFIG_TEMPLATE
+
+        self.config = config
