257d74309df03a74838cecf6510fb698aa20ab91
Abhay Gupta <bhgupta13@gmail.com>
2018-10-24 15:34:58 +0530

    searchdocs_test: Change http links to https
    This changes http:// links in constants.py file in plugins folder
    and searchdocs_test.py file in tests folder to https:// links.
    
    Closes https://github.com/coala/corobo/issues/579

--- plugins/constants.py
+++ plugins/constants.py
@@ -1,6 +1,6 @@
 
-API_DOCS = 'http://api.coala.io/en/latest'
-USER_DOCS = 'http://docs.coala.io/en/latest'
+API_DOCS = 'https://api.coala.io/en/latest'
+USER_DOCS = 'https://docs.coala.io/en/latest'
 
 MAX_MSG_LEN = 1000
 MAX_LINES = 20
--- tests/searchdocs_test.py
+++ tests/searchdocs_test.py
@@ -6,9 +6,9 @@ class SearchDocsTest(IsolatedTestCase):
     def test_search_cmd(self):
         self.assertCommand(
             '!search api search string',
-            'http://api.coala.io/en/latest/search.html?q=search+string')
+            'https://api.coala.io/en/latest/search.html?q=search+string')
         self.assertCommand(
             '!search user search string',
-            'http://docs.coala.io/en/latest/search.html?q=search+string')
+            'https://docs.coala.io/en/latest/search.html?q=search+string')
         self.assertCommand('!search not matching',
                            'Invalid syntax')
