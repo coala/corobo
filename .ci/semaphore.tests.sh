707a43eb6a801e11299f5dc2107c9a622b538035
Sladyn <gunnerforlife00@gmail.com>
2019-03-18 21:44:20 +0530

    semaphore.tests.sh: Add latest repo source
    Installed the latest coala and coala-bears
    repo in semaphore.tests.sh

--- .ci/semaphore.tests.sh
+++ .ci/semaphore.tests.sh
@@ -1,6 +1,6 @@
 set -e -x
 
-pip install coala-bears
+pip install git+https://github.com/coala/coala-bears.git@master
 pip install -r requirements.txt
 pip install -r test-requirements.txt
 sed -i.bak '/bears = GitCommitBear/d' .coafile
