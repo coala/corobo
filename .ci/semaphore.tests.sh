9c23addcdfe066c1818a356edbc3066358e854ba
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2019-03-19 23:45:33 +0530

    semaphore.tests.sh: Use coala master
    Closes https://github.com/coala/corobo/issues/654

--- .ci/semaphore.tests.sh
+++ .ci/semaphore.tests.sh
@@ -1,5 +1,6 @@
 set -e -x
 
+pip install git+https://github.com/coala/coala.git@master
 pip install git+https://github.com/coala/coala-bears.git@master
 pip install -r requirements.txt
 pip install -r test-requirements.txt
